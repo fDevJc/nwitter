b7c1dbc8cae285563dbbf8cc2163f1e61719b62c
fDevJc <fDevJc@gmail.com>
2021-03-04 22:16:40 +0900

    add firebase auth(google,github)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
-import AppRouter from './Router';
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+
+import { authService } from 'fBase';
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} nwitter</footer>
+    </>
+  );
 }
 
 export default App;
