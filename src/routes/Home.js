81bbff9a4e1efed537ab3faa23a9794f9bf9c4a4
fDevJc <fDevJc@gmail.com>
2021-03-07 17:15:27 +0900

    created nweets create and read functions

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,26 @@
 import { dbService } from 'fBase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
   const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection('nweets').get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection('nweets').add({
-      nweet,
+      text: nweet,
       createdAt: Date.now(),
     });
     setNweet('');
@@ -17,6 +31,7 @@ const Home = () => {
     } = event;
     setNweet(value);
   };
+  console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -27,8 +42,16 @@ const Home = () => {
           value={nweet}
           onChange={onChange}
         ></input>
-        <input type="submit" value="Nweet"></input>
+        <input type="submit" value="Nweet"></input>1
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+        ;
+      </div>
     </div>
   );
 };
